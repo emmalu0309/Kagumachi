086ac55245a8d6d8ba06bbdd25ebddd726f16c90
whj86 <whj906732@gmail.com>
2024-12-19 01:13:43 +0800

    1. MyKeep.jsx, MyKeepItem樣式改成tailwind版本
    2. 移除MyKeep.css

--- src/components/MyKeepItem.jsx
+++ src/components/MyKeepItem.jsx
@@ -1,66 +1,69 @@
-function MyKeepItem({itemName, imgSrc, itemPrice}) {
-    return (
-        <li className="MyKeepli">
-              <img
-                className="MyKeepDelImg"
-                src="https://s1.lativ.com.tw/images/btn_del.png"
-                alt=""
-              />
-              <a>
-                <img
-                  className="MyKeepImg"
-                //   alt="牛津長袖襯衫-男"
-                  src={imgSrc}
-                />
-              </a>
-              <h3 className="any_display_name">{itemName}</h3>
-              <h3 className="any_display_price">
-                <span>
-                  <span className="MyKeepCurrencySymbol">{itemPrice}</span>
-                </span>
-              </h3>
-              <table>
-                <tbody>
-                  <tr>
-                    <td className="MyKeepAlignL">
-                      <select className="MyKeepColorOpt">
-                        <option value="">顏色</option>
+import { Link } from "react-router-dom";
 
-                        <option value="白色">白色</option>
+function MyKeepItem({ itemName, itemSrc, imgSrc, itemPrice }) {
+  return (
+    <li className="w-48 h-72 justify-self-center relative text-center p-2 border border-solid border-gray-200">
+      <img
+        className="h-5 w-5 absolute top-2 left-2 hover:cursor-pointer"
+        src="https://s1.lativ.com.tw/images/btn_del.png"
+      />
+      {/* 點商品圖片會到該商品頁面 */}
+      <Link to={itemSrc}>
+        {/* 切換顏色應切換商品圖片(未完成) */}
+        <img className="h-36 w-36 ml-3.5 hover:cursor-pointer" src={imgSrc} />
+      </Link>
+      <h3 className="text-xs my-2.5 text-gray-500 truncate ">{itemName}</h3>
+      <h3 className="text-xs m-3 text-gray-400">{itemPrice}</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <select className="text-sm w-24 border border-solid border-gray-300 rounded text-gray-400">
+                <option value="" disabled selected>
+                  顏色
+                </option>
+                {/* 再想顏色要怎放進來 */}
+                <option value="白色">白色</option>
 
-                        <option value="卡其">卡其</option>
+                <option value="卡其">卡其</option>
 
-                        <option value="軍綠">軍綠</option>
+                <option value="軍綠">軍綠</option>
 
-                        <option value="藍色">藍色</option>
+                <option value="藍色">藍色</option>
 
-                        <option value="灰藍">灰藍</option>
+                <option value="灰藍">灰藍</option>
 
-                        <option value="藏青">藏青</option>
-                      </select>
-                    </td>
-                    <td className="MyKeepAlignR">
-                      <select className="MyKeepSizeOpt">
-                        <option value="">尺寸</option>
-                      </select>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className="MyKeepAlignL">
-                      <select className="MyKeepStockOpt">
-                        <option value="">數量</option>
-                      </select>
-                    </td>
-                    <td className="MyKeepAlignR">
-                      <button className="MyKeepCheckBtn">
-                        <span>選購</span>
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-    );
+                <option value="藏青">藏青</option>
+              </select>
+            </td>
+            <td>
+              <select className="text-sm w-16 float-right border border-solid border-gray-300 rounded text-gray-400">
+                <option disabled selected>
+                  尺寸
+                </option>
+                {/* 再想尺寸要怎放進來 */}
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <select className="text-sm w-16 float-left mt-2 border border-solid border-gray-300 rounded text-gray-400">
+                <option disabled selected>
+                  數量
+                </option>
+                {/* 再想數量要怎放進來 */}
+              </select>
+            </td>
+            <td>
+              <button className="ml-7 px-1 py-0 mt-1 border border-gray-300 rounded-md bg-gray-100 text-gray-600 hover:bg-[#E4D8CC] hover:text-white">
+                <span>選購</span>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </li>
+  );
 }
 
 export default MyKeepItem;
--- src/pages/MyKeep.jsx
+++ src/pages/MyKeep.jsx
@@ -1,20 +1,53 @@
-import "./MyKeep.css";
 import MyKeepItem from "../components/MyKeepItem";
 function MyKeep() {
   return (
-    <div className="MyKeepWrap">
-      <div className="MyKeepWrap2">
-        <div>
-          <ul className="MyKeepul">
-            <MyKeepItem itemName="牛津長袖襯衫-男" itemPrice="NT$399" imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg" />
-            <MyKeepItem itemName="柔軟舒適圓領毛衣-童" itemPrice="NT$450" imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg" />
-            <MyKeepItem itemName="牛津長袖襯衫-男" itemPrice="NT$399" imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg" />
-            <MyKeepItem itemName="柔軟舒適圓領毛衣-童" itemPrice="NT$450" imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg" />
-            <MyKeepItem itemName="牛津長袖襯衫-男" itemPrice="NT$399" imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg" />
-            <MyKeepItem itemName="柔軟舒適圓領毛衣-童" itemPrice="NT$450" imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg" />
-          </ul>
-        </div>
-      </div>
+    <div>
+      <ul className="mx-44 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-y-4 gap-x-40 justify-evenly">
+        <MyKeepItem
+          itemName="牛津長袖襯衫-男"
+          itemPrice="NT$399"
+          imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg"
+          // e.g.
+          // itemColor={["紅色", "藍色", "黑色"]}
+          // itemSize={["s", "m", "l", "xl"]}
+          // itemQty={10}
+        />
+        <MyKeepItem
+          itemName="柔軟舒適圓領毛衣-童"
+          itemPrice="NT$450"
+          imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg"
+        />
+        <MyKeepItem
+          itemName="牛津長袖襯衫-男"
+          itemPrice="NT$399"
+          imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg"
+        />
+        <MyKeepItem
+          itemName="柔軟舒適圓領毛衣-童"
+          itemPrice="NT$450"
+          imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg"
+        />
+        <MyKeepItem
+          itemName="牛津長袖襯衫-男"
+          itemPrice="NT$399"
+          imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg"
+        />
+        <MyKeepItem
+          itemName="柔軟舒適圓領毛衣-童"
+          itemPrice="NT$450"
+          imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg"
+        />
+        <MyKeepItem
+          itemName="牛津長袖襯衫-男"
+          itemPrice="NT$399"
+          imgSrc="https://s1.lativ.com.tw/i/66419/66419011/6641901_360.jpg"
+        />
+        <MyKeepItem
+          itemName="柔軟舒適圓領毛衣-童"
+          itemPrice="NT$450"
+          imgSrc="	https://s2.lativ.com.tw/i/67150/67150011/6715001_360.jpg"
+        />
+      </ul>
     </div>
   );
 }
