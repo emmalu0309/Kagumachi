9a5db5c91e3fa85a6d9542dd9dc1aa88bebc72fe
emmalu0309 <emmalu0309@icloud.com>
2024-12-28 08:19:35 +0800

    新增客服頁

---
+++ src/pages/CustomerSupport.jsx
@@ -0,0 +1,18 @@
+
+
+const CustomerSupport = () => {
+    return (
+        <div className="w-[50%] mx-auto text-center mt-10">
+            <div>
+                <div>歷史訊息</div>
+                <div>
+                    <input placeholder="請輸入訊息"/>
+                    <button>送出</button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default CustomerSupport;
