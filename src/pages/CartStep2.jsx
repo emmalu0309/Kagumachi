fad7fa58548cbd1c452a203bf90e738bf0453b88
Jesseli016 <chongjo320@gmail.com>
2025-01-09 09:49:40 +0800

    新增收件人表單元件

--- src/pages/CartStep2.jsx
+++ src/pages/CartStep2.jsx
@@ -5,12 +5,12 @@ import OrderSummary from "../components/OrderSummary";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 
-function App() {
+function CartStep2() {
     const [currentStep, setCurrentStep] = useState(2);
     const [selectedPayment, setSelectedPayment] = useState("");
 
     const paymentOptions = [
-        { id: "cod", label: "宅配貨到付款" },
+        { id: "cod", label: "宅配貨到付款(限台灣本島)" },
         { id: "credit", label: "信用卡付款" },
     ];
 
@@ -22,56 +22,57 @@ function App() {
     };
 
     return (
-        <div className="max-w-4xl mx-auto p-6 bg-gray-100 min-h-screen">
-
+        <div>
             {/* StepIcon */}
             <StepIcon step={currentStep.toString()} />
 
-            {/* 付款方式選擇 */}
-            <PaymentOptions
-                options={paymentOptions}
-                selectedOption={selectedPayment}
-                onSelect={setSelectedPayment}
-            />
+            <div className="max-w-4xl mx-auto p-6 bg-gray-100 min-h-screen">
 
-            {/* 訂單摘要 */}
-            <OrderSummary
-                itemsCount={orderData.itemsCount}
-                totalPrice={orderData.totalPrice}
-                shippingFee={orderData.shippingFee}
-                discount={orderData.discount}
-            />
+                {/* 付款方式選擇 */}
+                <PaymentOptions
+                    options={paymentOptions}
+                    selectedOption={selectedPayment}
+                    onSelect={setSelectedPayment}
+                />
 
-            {/* 按鈕 */}
-            <div className="flex justify-between mt-6">
+                {/* 訂單摘要 */}
+                <OrderSummary
+                    itemsCount={orderData.itemsCount}
+                    totalPrice={orderData.totalPrice}
+                    shippingFee={orderData.shippingFee}
+                    discount={orderData.discount}
+                />
 
-                <Link to="/ShoppingCart">
-                    <Button
-                        label="返回"
-                        onClick={() => setCurrentStep((prev) => Math.max(prev - 1, 1))}
+                {/* 按鈕 */}
+                <div className="flex justify-between mt-6">
+
+                    <Link to="/ShoppingCart">
+                        <Button
+                            label="返回"
+                            onClick={() => setCurrentStep((prev) => Math.max(prev - 1, 1))}
                         >
-                    </Button>
-                </Link>
+                        </Button>
+                    </Link>
 
-                <Link to={
-                    selectedPayment === "cod"
-                        ? "/CartStep3COD"
-                        : selectedPayment === "credit"
-                            ? "/CartStep3Credit"
-                            : "#"  // 未選擇付款方式時，導航設為 "#"（空連結）
-                }>
-                    <Button
-                        label="下一步"
-                        onClick={() => setCurrentStep((prev) => Math.min(prev + 1, 4))}
+                    <Link to={
+                        selectedPayment === "cod"
+                            ? "/CartStep3COD"
+                            : selectedPayment === "credit"
+                                ? "/CartStep3Credit"
+                                : "#"  // 未選擇付款方式時，導航設為 "#"（空連結）
+                    }>
+                        <Button
+                            label="下一步"
+                            onClick={() => setCurrentStep((prev) => Math.min(prev + 1, 4))}
                         // disabled={!selectedPayment}  // 未選擇付款方式時，Link不會觸發
                         >
-                    </Button>
-                </Link>
+                        </Button>
+                    </Link>
 
-
+                </div>
             </div>
         </div>
     );
 }
 
-export default App;
+export default CartStep2;
--- src/pages/CartStep3Credit.jsx
+++ src/pages/CartStep3Credit.jsx
@@ -3,39 +3,40 @@ import StepIcon from "../components/StepIcon";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 
-function App() {
+function CartStep3Credit() {
     const [currentStep, setCurrentStep] = useState(3);
     const [selectedPayment, setSelectedPayment] = useState("");
 
     return (
-        <div className="max-w-4xl mx-auto p-6 bg-gray-100 min-h-screen">
-
+        <div>
             {/* StepIcon */}
             <StepIcon step={currentStep.toString()} />
 
-            {/* 按鈕 */}
-            <div className="flex justify-between mt-6">
+            <div className="max-w-4xl mx-auto p-6 bg-gray-100 min-h-screen">
 
-                <Link to="/CartStep2">
-                    <Button
-                        label="返回"
-                        onClick={() => setCurrentStep((prev) => Math.max(prev - 1, 1))}
-                    >
-                    </Button>
-                </Link>
+                {/* 按鈕 */}
+                <div className="flex justify-between mt-6">
 
-                <Link to="/">
-                    <Button
-                        label="下一步"
-                        onClick={() => setCurrentStep((prev) => Math.min(prev + 1, 4))}
-                        className={selectedPayment}>
-                    </Button>
-                </Link>
+                    <Link to="/CartStep2">
+                        <Button
+                            label="返回"
+                            onClick={() => setCurrentStep((prev) => Math.max(prev - 1, 1))}
+                        >
+                        </Button>
+                    </Link>
 
+                    <Link to="/">
+                        <Button
+                            label="下一步"
+                            onClick={() => setCurrentStep((prev) => Math.min(prev + 1, 4))}
+                            className={selectedPayment}>
+                        </Button>
+                    </Link>
 
+                </div>
             </div>
         </div>
     );
 }
 
-export default App;
+export default CartStep3Credit;
