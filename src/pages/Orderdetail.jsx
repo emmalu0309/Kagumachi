4f80c530f2b21a4512177d2e22ce5ff75a995395
ysl <yslsy224@gmail.com>
2025-01-28 22:32:05 +0800

    feat: 新增收件資料

--- src/pages/Orderdetail.jsx
+++ src/pages/Orderdetail.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 // import { AuthContext } from "../context/AuthContext";
 
@@ -8,46 +8,74 @@ const Orderdetail = () => {
 
   const { orderserial } = useParams();
   const [orderData, setOrderData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [deliveryData, setDeliveryData] = useState(null);
+  const [loadingOrder, setLoadingOrder] = useState(true);
+  const [loadingDelivery, setLoadingDelivery] = useState(true);
 
   useEffect(() => {
     const fetchOrderData = async () => {
-      console.log("orderserial from useParams:", orderserial);
-      const response = await fetch(
-        `http://localhost:8080/orderdetail/${orderserial}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (!response.ok) {
-        const errorText = await response.text();
-        console.error("Error Response:", errorText);
-        throw new Error(
-          `抓取訂單資料失敗: ${response.status} ${response.statusText} - ${errorText}`
+      try {
+        const response = await fetch(
+          `http://localhost:8080/orderdetail/${orderserial}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
         );
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error("Error Response:", errorText);
+          throw new Error(
+            `抓取訂單資料失敗: ${response.status} ${response.statusText} - ${errorText}`
+          );
+        }
+        const data = await response.json();
+        setOrderData(data);
+      } catch (error) {
+        console.error("Fetch Order Error:", error.message);
+      } finally {
+        setLoadingOrder(false);
       }
-      const data = await response.json();
-      console.log("回傳資料:", data);
-      setOrderData(data);
-      setLoading(false);
     };
     fetchOrderData();
   }, [orderserial]);
 
-  if (loading) {
+  useEffect(() => {
+    const fetchDeliveryData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/orderdetail/delivery/${orderserial}`
+        );
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error("Error Response:", errorText);
+          throw new Error(
+            `抓取運送資料失敗: ${response.status} ${response.statusText} - ${errorText}`
+          );
+        }
+        const data = await response.json();
+        setDeliveryData(data);
+      } catch (error) {
+        console.error("Fetch Delivery Error:", error.message);
+      } finally {
+        setLoadingDelivery(false);
+      }
+    };
+    fetchDeliveryData();
+  }, [orderserial]);
+
+  if (loadingOrder || loadingDelivery) {
     return <div>載入中...</div>;
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
-      {/* 外層容器，水平垂直置中 */}
+    <div className="flex items-center justify-center py-16">
       <div className="w-3/4 space-y-6 mt-0">
         {/* 訂單編號區塊 */}
-        <div className="text-left  text-gray-400 mt-8">
-          您的訂單編號為：<span className="font-semibold">{orderserial}</span>
+        <div className="mb-2 font-bold text-gray-600">
+          訂單編號：<span className="font-semibold">{orderserial}</span>
         </div>
 
         {/* 購物明細區塊 */}
@@ -93,9 +121,35 @@ const Orderdetail = () => {
         {/* 寄送資料區塊 */}
         <div className="border border-gray-400 rounded-lg p-4 text-left text-gray-600">
           <div className="mb-4 font-bold">付款方式與寄送資料</div>
-          <span className="block">付款方式</span>
-          <span className="block">訂購姓名</span>
-          <span className="block">取貨地址</span>
+          <hr />
+          <span className="mt-4 block font-bold">
+            付款方式：
+            <span className="font-normal">{deliveryData.shippingmethod}</span>
+          </span>
+          <span className="block font-bold">
+            取貨人姓名：
+            <span className="font-normal">{deliveryData.recipient}</span>
+          </span>
+          <span className="block font-bold">
+            取貨地址：
+            <span className="font-normal">
+              {deliveryData.ordercity}
+              {deliveryData.district}
+              {deliveryData.address}
+            </span>
+          </span>
+          <span className="block font-bold">
+            取貨電話：<span className="font-normal">{deliveryData.phone}</span>
+          </span>
+        </div>
+
+        <div className="flex items-center justify-center mt-4 space-y-2">
+          <Link
+            to="/MemberInfo/MyOrders"
+            className="px-8 py-2 bg-[#5e3b25] text-white rounded-md hover:bg-[#c3a789] w-[120px] text-center"
+          >
+            返回會員頁面
+          </Link>
         </div>
       </div>
     </div>
